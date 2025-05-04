6571946d15643035d632df2d1b925f22a085a564
KiwamiXq1031 <ss9818023@gmail.com>
2025-05-04 21:00:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,362 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
+const axios = require("axios")
+const pino = require('pino');
+const { setTimeout: sleep } = require('timers/promises');
+const crypto = require("crypto");
+const FormData = require('form-data');
+const QRCode = require('qrcode');
+const bodyParser = require('body-parser');
+const { ImageUploadService } = require('node-upload-images')
+const { default: makeWaSocket, useMultiFileAuthState, fetchLatestBaileysVersion } = require('@whiskeysockets/baileys');
+
+// Middleware
+app.enable("trust proxy");
+app.set("json spaces", 2);
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Endpoint untuk servis dokumen HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/api', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get("/api/downloader/tiktok", async (req, res) => {
+  const { url } = req.query;
+  if (!url) return res.status(400).json({ error: "URL is required." });
+
+  try {
+    const { tiktokdl } = require("tiktokdl");
+    const data = await tiktokdl(url);
+    if (!data) return res.status(404).json({ error: "No data found." });
+    res.json({ status: true, creator: "Rafael", result: data });
+  } catch (e) {
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+
+
+app.get("/api/tools/translate", async (req, res) => {
+  const { text } = req.query;
+  if (!text) return res.status(400).json({ error: "Text is required." });
+
+  try {
+    const response = await axios.get(`https://api.siputzx.my.id/api/tools/translate`, {
+      params: { text: text, source: "auto", target: "id" }
+    });
+    res.json({ status: true, creator: "Rafael", result: response.data.translatedText });
+  } catch {
+    res.status(500).json({ error: "An error occurred while processing the translation." });
+  }
+});
+
+
+app.get("/api/downloader/spotify", async (req, res) => {
+    const { url } = req.query;
+    if (!url) return res.status(400).json({ error: "Url is required." });
+    try {
+        const response = await axios.get(`https://api.siputzx.my.id/api/d/spotify?url=${url}`);
+        const data = response.data;
+        if (!data.metadata || !data.download) {
+            return res.status(500).json({ error: "Invalid response from the external API." });
+        }
+        res.json({
+            status: true,
+            creator: "Rafael",
+            result: {
+                artis: data.metadata.artist,
+                judul: data.metadata.name,
+                rilis: data.metadata.releaseDate,
+                thumbnail: data.metadata.cover_url,
+                download_url: data.download
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch data from the external API." });
+    }
+});
+
+app.get("/api/downloader/ytmp3", async (req, res) => {
+    const { url } = req.query;
+    if (!url) return res.status(400).json({ error: "Url is required." });
+
+    try {
+        const response = await axios.get(`https://api.siputzx.my.id/api/d/youtube?q=${url}`);
+        const data = response.data;
+
+        res.json({
+            status: true,
+            creator: "Rafael",
+            result: {
+                Judul: data.data.title,
+                thumbnail: data.data.thumbnailUrl,
+                durasi: data.data.duration,
+                UrlDownload: data.data.sounds
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching data." });
+    }
+});
+
+app.get("/api/downloader/ytmp4", async (req, res) => {
+    const { url } = req.query;
+    if (!url) return res.status(400).json({ error: "Url is required." });
+
+    try {
+        const response = await axios.get(`https://api.siputzx.my.id/api/d/youtube?q=${url}`);
+        const data = response.data;
+
+        res.json({
+            status: true,
+            creator: "Rafael",
+            result: {
+                Judul: data.data.title,
+                thumbnail: data.data.thumbnailUrl,
+                durasi: data.data.duration,
+                UrlDownload: data.data.video
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred while fetching data." });
+    }
+});
+
+
+function convertCRC16(str) {
+    let crc = 0xFFFF;
+    const strlen = str.length;
+
+    for (let c = 0; c < strlen; c++) {
+        crc ^= str.charCodeAt(c) << 8;
+
+        for (let i = 0; i < 8; i++) {
+            if (crc & 0x8000) {
+                crc = (crc << 1) ^ 0x1021;
+            } else {
+                crc = crc << 1;
+            }
+        }
+    }
+
+    let hex = crc & 0xFFFF;
+    hex = ("000" + hex.toString(16).toUpperCase()).slice(-4);
+
+    return hex;
+}
+
+function generateTransactionId() {
+    return crypto.randomBytes(5).toString('hex').toUpperCase()
+}
+
+function generateExpirationTime() {
+    const expirationTime = new Date();
+    expirationTime.setMinutes(expirationTime.getMinutes() + 30);
+    return expirationTime;
+}
+
+async function elxyzFile(buffer) {
+    return new Promise(async (resolve, reject) => {
+        try {
+const service = new ImageUploadService('pixhost.to');
+let { directLink } = await service.uploadFromBinary(buffer, 'skyzo.png');
+            resolve(directLink);
+        } catch (error) {
+            console.error('🚫 Upload Failed:', error);
+            reject(error);
+        }
+    });
+}
+
+async function generateQRIS(amount) {
+    try {
+        let qrisData = "code qris lu";
+
+        qrisData = qrisData.slice(0, -4);
+        const step1 = qrisData.replace("010211", "010212");
+        const step2 = step1.split("5802ID");
+
+        amount = amount.toString();
+        let uang = "54" + ("0" + amount.length).slice(-2) + amount;
+        uang += "5802ID";
+
+        const result = step2[0] + uang + step2[1] + convertCRC16(step2[0] + uang + step2[1]);
+
+        const buffer = await QRCode.toBuffer(result);
+
+        const uploadedFile = await elxyzFile(buffer);
+
+        return {
+            transactionId: generateTransactionId(),
+            amount: amount,
+            expirationTime: generateExpirationTime(),
+            qrImageUrl: uploadedFile
+        };
+    } catch (error) {
+        console.error('Error generating and uploading QR code:', error);
+        throw error;
+    }
+}
+
+async function createQRIS(amount, codeqr) {
+    try {
+        let qrisData = codeqr;
+
+        qrisData = qrisData.slice(0, -4);
+        const step1 = qrisData.replace("010211", "010212");
+        const step2 = step1.split("5802ID");
+
+        amount = amount.toString();
+        let uang = "54" + ("0" + amount.length).slice(-2) + amount;
+        uang += "5802ID";
+
+        const result = step2[0] + uang + step2[1] + convertCRC16(step2[0] + uang + step2[1]);
+
+        const buffer = await QRCode.toBuffer(result);
+
+        const uploadedFile = await elxyzFile(buffer);
+
+        return {
+            idtransaksi: generateTransactionId(),
+            jumlah: amount,
+            expired: generateExpirationTime(),
+            imageqris: { 
+            url: uploadedFile
+            }
+        };
+    } catch (error) {
+        console.error('Error generating and uploading QR code:', error);
+        throw error;
+    }
+}
+
+async function checkQRISStatus() {
+    try {
+        const apiUrl = `https://gateway.okeconnect.com/api/mutasi/qris/isi pakai merchant orkut/apikey orkut`;
+        const response = await axios.get(apiUrl);
+        const result = response.data;
+        const data = result.data;
+        let capt = '*Q R I S - M U T A S I*\n\n';
+        if (data.length === 0) {
+            capt += 'Tidak ada data mutasi.';
+        } else {
+            data.forEach(entry => {
+                capt += '```Tanggal:```' + ` ${entry.date}\n`;
+                capt += '```Issuer:```' + ` ${entry.brand_name}\n`;
+                capt += '```Nominal:```' + ` Rp ${entry.amount}\n\n`;
+            });
+        }
+        return capt;
+    } catch (error) {
+        console.error('Error checking QRIS status:', error);
+        throw error;
+    }
+}
+
+app.get('/orderkuota/createpayment', async (req, res) => {
+    const { amount, codeqr } = req.query;
+    try {
+        const qrData = await createQRIS(amount, codeqr);
+        res.status(200).json({
+                status: true,
+                result: qrData
+        });      
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+    
+app.get('/orderkuota/cekstatus', async (req, res) => {
+    const { merchant, keyorkut } = req.query;
+        try {
+        const apiUrl = `https://gateway.okeconnect.com/api/mutasi/qris/${merchant}/${keyorkut}`;
+        const response = await axios.get(apiUrl);
+        const result = await response.data;
+                // Check if data exists and get the latest transaction
+        const latestTransaction = result.data && result.data.length > 0 ? result.data[0] : null;
+                if (latestTransaction) {
+         res.status(200).json({
+            status: true, 
+            result: latestTransaction
+        })
+        } else {
+            res.json({ message: "No transactions found." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.get('/orderkuota/ceksaldo', async (req, res) => {
+    const { merchant, keyorkut } = req.query;
+        try {
+        const apiUrl = `https://gateway.okeconnect.com/api/mutasi/qris/${merchant}/${keyorkut}`;
+        const response = await axios.get(apiUrl);
+        const result = await response.data;
+                // Check if data exists and get the latest transaction
+        const latestTransaction = result.data && result.data.length > 0 ? result.data[0] : null;
+                if (latestTransaction) {
+         res.status(200).json({
+            status: true, 
+            result: {
+            saldo_qris: latestTransaction.balance
+            }
+        })
+        } else {
+            res.json({ message: "No transactions found." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+})
+
+app.get("/api/downloader/spotifys", async (req, res) => {
+    try {
+        const { judul } = req.query;
+        if (!judul) {
+            return res.status(400).json({ error: "Masukkan judul lagu." });
+        }
+        const response = await axios.get(`https://api.siputzx.my.id/api/s/spotify?query=${encodeURIComponent(judul)}`);
+        const resultData = response.data.data[0];
+        if (!resultData) {
+            return res.status(404).json({ error: "Lagu tidak ditemukan." });
+        }
+        res.json({
+            status: true,
+            creator: "Rafael",
+            result: {
+                judul: resultData.title,
+                artis: resultData.artist.name,
+                thumbnail: resultData.thumbnail,
+                url: resultData.artist.external_urls.spotify
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Terjadi kesalahan pada server." });
+    }
+});
+
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Jalankan server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
